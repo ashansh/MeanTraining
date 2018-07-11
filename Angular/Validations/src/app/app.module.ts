62b202b76539cc770f2e089c6a6ef615fc132c01
= <lokhandeashansh@gmail.com>
2018-07-11 14:54:57 +0530

    Form with Angular Validations

--- Angular/Validations/src/app/app.module.ts
+++ Angular/Validations/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule , FormsModule
